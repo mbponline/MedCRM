67e6ebb570044511ea025daa4911dd226f2f5691
Zhanbyrbayev Nuraly <nuraly.zhanbyrbayev@gmail.com>
2019-05-27 18:50:40 +0600

    initial commit

---
+++ MedCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
